a45d0edd727efa1344e13883e5c3d583d257ce7f
gqlNU <guangquan.li@northumbria.ac.uk>
2023-04-28 15:40:33 +0100

    figures

--- inst/scripts/disaggregating.R
+++ inst/scripts/disaggregating.R
@@ -11,30 +11,28 @@ if (!dir.exists('forecast')) dir.create('forecast')
 
 ###  user inputs
 setting <- list()
-setting$imodel <- 2
+setting$imodel <- 3
 setting$fit_ts <- c(1,20)
-setting$horizons <- 5
+setting$horizon <- 5
 setting$use_pmean <- FALSE
+if (setting$imodel==1) setting$use_pmean <- TRUE
 setting$recollect <- TRUE
 
 alldata_file <- '../../alldata.rds'
 alldata <- get_all_ww_prev_data(save_file=NULL,load_file=alldata_file)
 
-for (horizon in  setting$horizons) {
 ###  get the list of files containing the fits
-    setting$draw_files <- get_nimble_files(setting)
-    setting$ndraws <- length(setting$draw_files)
+setting$draw_files <- get_nimble_files(setting)
+setting$ndraws <- length(setting$draw_files)
 
 ###  get disaggregated prevalence from each fit
-    ds <- get_dis_prev(setting,alldata)
+ds <- get_dis_prev(setting,alldata)
 
 ###  summarise disaggregated prevalence
-    my_print('summarising disaggregated prevalence')
-    forecast <- summarise_dis_prev(ds,setting)
+my_print('summarising disaggregated prevalence')
+forecast <- summarise_dis_prev(ds,setting)
 
 ###  saving disaggregated prevalence
-    my_print('saving disaggregated prevalence')
-    save(file=forecast$save_file,forecast)
-    my_print('done')
-}  #  next horizon
-
+my_print('saving disaggregated prevalence')
+save(file=forecast$save_file,forecast)
+my_print('done')
--- inst/scripts/summarising.R
+++ inst/scripts/summarising.R
@@ -3,10 +3,13 @@
 
 rm(list=ls())
 library(wwprev)  #  run install_wwprev.R to install this package
+library(ComplexHeatmap)
+library(circlize)
 
 workdir <- '/Volumes/WorkSpace/OnGitHub/wwprev/forTesting/results/nimble_keep/forecast/'
 setwd(workdir)
 
+save_pdf <- TRUE  #  save figures to pdf?
 imodels <- 2
 fit_ts <- c(1,20)
 horizon <- 20
@@ -19,7 +22,7 @@ alldata <- get_all_ww_prev_data(save_file=NULL,load_file=alldata_file)
 t_ids <- (1:horizon) + fit_ts[2]
 obs <- alldata$p_mn[,t_ids]
 
-
+###  overall summary
 for (imodel in imodels) {
   file <- paste0('summary_nimble_multiscale_m',imodel,'_t',fit_ts[1],'-t',fit_ts[2],'h',horizon)
   if (use_pmean | imodel==1) {
@@ -32,3 +35,38 @@ for (imodel in imodels) {
   print(file)
   print(forecast_summary(pred_mean=pred[2,,],pred_ci=pred[c(1,3),,],obs))
 }
+
+###  define color scheme for figures
+diff <- pred[2,,] - obs
+rownames(diff) <- NULL
+rg <- round(range(c(diff)),digits=1)
+m <- max(abs(rg))
+#rg <- c(-m, 0, m)
+rg <- c(rg[1],0,rg[2])
+#rg <- c(-1.5,0,1.5)
+col_fun <- colorRamp2(rg, c("brown", "lightgreen", "darkblue"))
+
+###  disaggregated vs observed: scatterplot
+l <- range(pred[2,,],obs)
+#tiff(file='../../../figures/scatter.tif',height=500,width=500)
+tiff(file='../../../figures/scatter.tif',res=200,width=6,height=6,unit='in')
+plot(pred[2,,],obs,pch=19,cex=0.6
+    ,xlab='Disaggregated logit prevalence'
+    ,ylab='Estimated logit prevalence',xlim=l,ylim=l
+    ,col=col_fun(diff))
+points(pred[2,,],obs,lwd=0.02,cex=0.65)
+abline(0,1,col=2,lwd=2)
+dev.off()
+
+###  heatmap of difference
+tiff(file='../../../figures/heatmap.tif',res=200,width=6,height=6,unit='in')
+Heatmap(diff,cluster_rows=FALSE,cluster_columns=FALSE
+       ,col=col_fun
+       ,heatmap_legend_param=list(title='',legend_height=unit(10,'cm')
+                                 ,labels_gp = gpar(fontsize = 15))
+       ,column_title='Difference in logit prevalence: Disaggregated - Observed'
+       ,row_title='Lower Tier Local Authorities')
+dev.off()
+
+###  choropleth of LTLA RMSE averaged across weeks
+md <- apply(diff,1,RMSE)
